a6e6d296a3750c5a56df4c15c1ab2b0db4126939
sanjeev200009 <sanjaysanjeev2000@gmail.com>
2025-09-29 13:15:43 +0530

    Implement Ratelimiter for the Api

---
+++ backend/src/config/upstash.js
@@ -0,0 +1,13 @@
+const { Ratelimit } = require("@upstash/ratelimit");
+const { Redis } = require("@upstash/redis");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+// create a ratelimiter that allows 100 requests per 15 minutes
+const ratelimit = new Ratelimit({
+  redis: Redis.fromEnv(),
+  limiter: Ratelimit.slidingWindow(100, "15 m"),
+});
+
+module.exports = { ratelimit };

8caa34130523ff8516ff7fa8becd18d90a7c047b
sanjeev200009 <sanjaysanjeev2000@gmail.com>
2025-10-02 23:50:41 +0530

    feat: enhance note management with rate limiting, delete functionality, and axios instance setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors({
-    origin: 'http://localhost:5173', 
+    origin: ['http://localhost:5173', 'http://localhost:5174'], 
 }));
 
 // Middleware for JSON parsing
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,13 @@ import {Routes, Route} from "react-router"
 import HomePage from "./pages/HomePage"
 import CreatePage from "./pages/CreatePage"
 import NoteDetailPage from "./pages/NoteDetailPage"
+import { Toaster } from "react-hot-toast"
 
 
 const App = () => {
   return (
     <div>
+        <Toaster position="top-right" />
         <Routes>
             <Route path="/" element={<HomePage/>} />
              <Route path="/create" element={<CreatePage/>} />
